b576ef86cec06d5fb14ef3672fab733331e08fc3
Ben Cooper <ben.f.cooper@gmail.com>
2021-09-26 18:38:02 +0100

    Initial commit

---
+++ src/org/MrGhostlyOrb/PoetryAnalysis/Main.java
@@ -0,0 +1,8 @@
+package org.MrGhostlyOrb.PoetryAnalysis;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Poetry Analysis...");
+    }
+}
