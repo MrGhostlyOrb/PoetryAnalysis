dde89a90b33ecfb090306f6ec6cd4e870b2c46f0
Ben Cooper <ben.f.cooper@gmail.com>
2022-02-18 11:18:50 +0000

    feat(ALGO): Added logic for the Nysiis algorithm

--- src/org/MrGhostlyOrb/PoetryAnalysis/Poem.java
+++ src/org/MrGhostlyOrb/PoetryAnalysis/Poem.java
@@ -2,7 +2,7 @@ package org.MrGhostlyOrb.PoetryAnalysis;
 
 import com.diogonunes.jcolor.Ansi;
 import com.diogonunes.jcolor.Attribute;
-import org.apache.commons.codec.language.Soundex;
+import org.apache.commons.codec.language.Nysiis;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -125,7 +125,7 @@ public class Poem {
 
     public String createRhymeScheme() {
         StringBuilder rhymeScheme = new StringBuilder();
-        Soundex soundex = new Soundex();
+        Nysiis nysiis = new Nysiis();
         char[] alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
         int alphabetCounter = 0;
         for (Stanza stanza : stanzas) {
@@ -133,7 +133,7 @@ public class Poem {
             LinkedHashMap<String, String> sounds = new LinkedHashMap<>();
             for (Line line : stanza.getLines()) {
                 String lastWord = line.getLastWord();
-                String sound = soundex.soundex(lastWord);
+                String sound = nysiis.encode(lastWord);
                 sounds.put(lastWord, sound);
             }
             //Change this value to change algorithm
@@ -143,7 +143,7 @@ public class Poem {
                 StringBuilder stringBuilder = new StringBuilder();
 
                 //For soundex analysis
-                String currentSound = values.get(i).substring(1);
+                String currentSound = values.get(i).substring(values.get(i).length() -1);
 
                 if (i == 0) {
                     stringBuilder.append(alphabet[alphabetCounter]);
